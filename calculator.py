5d85939d2cf2228476a884485ebef915afa96caf
Szymon Żaba <szymonzaba18@gmail.com>
2024-01-03 18:03:02 +0100

    add seeing numbers and fixed grid

--- calculator.py
+++ calculator.py
@@ -1,28 +1,30 @@
 from tkinter import *
 
-root=Tk()
+root = Tk()
 root.title("Calculator")
 
 e = Entry(root, width=35, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3)
 
-def count():
-    answer = Label(root, text="1", font=("Helvetica", 8))
-    answer.grid(row=1, column=0)
 
-button1 = Button(root, text="1", padx=40, pady=20)
-button2 = Button(root, text="2", padx=40, pady=20)
-button3 = Button(root, text="3", padx=40, pady=20)
-button4 = Button(root, text="4", padx=40, pady=20)
-button5 = Button(root, text="5", padx=40, pady=20)
-button6 = Button(root, text="6", padx=40, pady=20)
-button7 = Button(root, text="7", padx=40, pady=20)
-button8 = Button(root, text="8", padx=40, pady=20)
-button9 = Button(root, text="9", padx=40, pady=20)
-buttonclear = Button(root, text="C", padx=80, pady=20)
-button0 = Button(root, text="0", padx=40, pady=20)
-buttonadd = Button(root, text="+", padx=40, pady=20)
-buttoneven = Button(root, text="=", padx=80, pady=20)
+def count(number):
+    #e.delete(0, END)
+    e.insert(0, number)
+
+
+button1 = Button(root, text="1", padx=40, pady=20, command=lambda: count(1))
+button2 = Button(root, text="2", padx=40, pady=20, command=lambda: count(2))
+button3 = Button(root, text="3", padx=40, pady=20, command=lambda: count(3))
+button4 = Button(root, text="4", padx=40, pady=20, command=lambda: count(4))
+button5 = Button(root, text="5", padx=40, pady=20, command=lambda: count(5))
+button6 = Button(root, text="6", padx=40, pady=20, command=lambda: count(6))
+button7 = Button(root, text="7", padx=40, pady=20, command=lambda: count(7))
+button8 = Button(root, text="8", padx=40, pady=20, command=lambda: count(8))
+button9 = Button(root, text="9", padx=40, pady=20, command=lambda: count(9))
+buttonclear = Button(root, text="C", padx=87, pady=20, command=lambda: count(1))
+button0 = Button(root, text="0", padx=40, pady=20, command=lambda: count(0))
+buttonadd = Button(root, text="+", padx=40, pady=20, command=lambda: count(1))
+buttoneven = Button(root, text="=", padx=87, pady=20, command=lambda: count(1))
 
 button1.grid(row=2, column=0)
 button2.grid(row=2, column=1)
@@ -33,10 +35,11 @@ button6.grid(row=3, column=2)
 button7.grid(row=4, column=0)
 button8.grid(row=4, column=1)
 button9.grid(row=4, column=2)
-buttonclear.grid(row=5, column=1)
+buttonclear.grid(row=5, column=1, columnspan=2)
 button0.grid(row=5, column=0)
-buttoneven.grid(row=6, column=1)
+buttoneven.grid(row=6, column=1, columnspan=2)
 buttonadd.grid(row=6, column=0)
 
 
 root.mainloop()
+
