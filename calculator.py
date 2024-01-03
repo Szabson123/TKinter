7cf0f7e2c5dfea0bc8c60d000a24a31287530ece
Szymon Żaba <szymonzaba18@gmail.com>
2024-01-03 18:18:12 +0100

    Simple adding

--- calculator.py
+++ calculator.py
@@ -9,7 +9,27 @@ e.grid(row=0, column=0, columnspan=3)
 
 def count(number):
     #e.delete(0, END)
-    e.insert(0, number)
+    current = e.get()
+    e.delete(0, END)
+    sth = e.insert(0, str(current) + str(number))
+    return sth
+
+
+def clear(first_number):
+    e.delete(0, END)
+
+
+def add():
+    first_number = e.get()
+    global f_num
+    f_num = int(first_number)
+    e.delete(0, END)
+
+
+def equal():
+    secound_number = e.get()
+    e.delete(0, END)
+    e.insert(0, f_num + int(secound_number))
 
 
 button1 = Button(root, text="1", padx=40, pady=20, command=lambda: count(1))
@@ -21,10 +41,10 @@ button6 = Button(root, text="6", padx=40, pady=20, command=lambda: count(6))
 button7 = Button(root, text="7", padx=40, pady=20, command=lambda: count(7))
 button8 = Button(root, text="8", padx=40, pady=20, command=lambda: count(8))
 button9 = Button(root, text="9", padx=40, pady=20, command=lambda: count(9))
-buttonclear = Button(root, text="C", padx=87, pady=20, command=lambda: count(1))
+buttonclear = Button(root, text="C", padx=87, pady=20, command=clear)
 button0 = Button(root, text="0", padx=40, pady=20, command=lambda: count(0))
-buttonadd = Button(root, text="+", padx=40, pady=20, command=lambda: count(1))
-buttoneven = Button(root, text="=", padx=87, pady=20, command=lambda: count(1))
+buttonadd = Button(root, text="+", padx=40, pady=20, command=add)
+buttoneven = Button(root, text="=", padx=87, pady=20, command=equal)
 
 button1.grid(row=2, column=0)
 button2.grid(row=2, column=1)
@@ -42,4 +62,3 @@ buttonadd.grid(row=6, column=0)
 
 
 root.mainloop()
-
