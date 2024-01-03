900dc44421357c5a8dd8c218a25c06f0679e91c0
Szabson123 <szymonzaba18@gmail.com>
2024-01-03 11:42:59 +0100

    color changes

--- buttons.py
+++ buttons.py
@@ -7,7 +7,7 @@ def clicked():
     myLabel.grid(row=2, column=1)
 
 myButton = Button(root, text="Click me!", state=DISABLED, padx=50, pady=50)
-myButton2 = Button(root, text="Click me!", command=clicked)
+myButton2 = Button(root, text="Click me!", command=clicked, fg="blue", bg="yellow")
 myButton.grid(row=0 ,column=0)
 myButton2.grid(row=1 ,column=0)
 
