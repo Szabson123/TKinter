8908bae9b6c625548beb01ddab416642d60a9b89
Szabson123 <szymonzaba18@gmail.com>
2024-01-10 12:27:02 +0100

    Basic view of main page

--- Mad_Libs_Generator.py
+++ Mad_Libs_Generator.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
 root = Tk()
+root.config(background='pink')
 
-title = Label(background='blue', text='Python Mad Libs Game')
+
+title = Label(text='Python Mad Libs Game', font=('Helvetica', 24))
 title.grid(row=0, column=0)
 
+button1 = Button(text='A memorable Game', background='blue', font=('Helvetica', 16))
+button1.grid(row=1, column=0, pady=100)
+
+button2 = Button(text='Ambitions', font=('Helvetica', 16), background='blue')
+button2.grid(row=2, column=0, pady=(0, 100))
 
 root.mainloop()
+
