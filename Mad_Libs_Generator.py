34324417fc3b663c5747dd6ac39242c4d6600ab4
Szabson123 <szymonzaba18@gmail.com>
2024-01-10 13:13:13 +0100

    Add second window and make basic inputs

--- Mad_Libs_Generator.py
+++ Mad_Libs_Generator.py
@@ -2,15 +2,61 @@ from tkinter import *
 
 root = Tk()
 root.config(background='pink')
+root.title('Libs Game')
 
+def story1():
+    
+    def text(name, enter_a_game, eneter_a_city, player, drink, snack):
+        #text = f"One fine day, my friend {name} and I planned to engage in a {enter_a_game} match in {eneter_a_city}. However, circumstances didn't allow us to participate. Instead, we ended up attending the game and saw our most admired athlete, {player}, in action. We sipped on {drink} and munched on some {snack}. It was truly an enjoyable experience! We are eagerly anticipating another opportunity to go and relish the moment."
+        pass
+    
+    def mainStory():
+        
+        newroot = Toplevel()
+        newroot.config(background='yellow')
+        
+        title = Label(newroot, text='A Memorable Day', font=('Helvetica', 20))
+        title.grid(row=0, column=0)
+        
+        name = Label(newroot, text='Name')
+        name.grid(row=1, column=0)
+        name_input = Entry(newroot, text='')
+        name_input.grid(row=1, column=1, pady=10)
 
-title = Label(text='Python Mad Libs Game', font=('Helvetica', 24))
+        enter_a_game = Label(newroot, text='Game')
+        enter_a_game.grid(row=2, column=0)
+        enter_a_game_input = Entry(newroot, text='')
+        enter_a_game_input.grid(row=2, column=1, pady=10)
+        
+        eneter_a_city = Label(newroot, text='City')
+        eneter_a_city.grid(row=3, column=0)
+        eneter_a_city_input = Entry(newroot, text='')
+        eneter_a_city_input.grid(row=3, column=1, pady=10)
+
+        player = Label(newroot, text='Player')
+        player.grid(row=4, column=0)
+        player_input = Entry(newroot, text='')
+        player_input.grid(row=4, column=1, pady=10)
+        
+        drink = Label(newroot, text='Drink')
+        drink.grid(row=5, column=0)
+        drink_input = Entry(newroot, text='')
+        drink_input.grid(row=5, column=1, pady=10)
+        
+        snack = Label(newroot, text='Snack')
+        snack.grid(row=6, column=0)
+        snack_input = Entry(newroot, text='')
+        snack_input.grid(row=6, column=1, pady=10)        
+            
+    return mainStory
+
+title = Label(root, text='Python Mad Libs Game', font=('Helvetica', 24))
 title.grid(row=0, column=0)
 
-button1 = Button(text='A memorable Game', background='blue', font=('Helvetica', 16))
+button1 = Button(root, text='A memorable Game', background='blue', font=('Helvetica', 16), command=story1())
 button1.grid(row=1, column=0, pady=100)
 
-button2 = Button(text='Ambitions', font=('Helvetica', 16), background='blue')
+button2 = Button(root, text='Ambitions', font=('Helvetica', 16), background='blue')
 button2.grid(row=2, column=0, pady=(0, 100))
 
 root.mainloop()
