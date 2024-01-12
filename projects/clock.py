a09129b0da8f8cce7e324ffe5963e242ef961e74
Szabson123 <szymonzaba18@gmail.com>
2024-01-12 09:23:31 +0100

    added buttons

--- projects/clock.py
+++ projects/clock.py
@@ -16,9 +16,19 @@ def clock():
     clock_label.after(1000, clock)
 
 
-clock_label = Label(root, text='', font=('Helvetica', 24), background='black', fg='yellow')
+clock_label = Label(root, text='', font=('Helvetica', 32), background='black', fg='yellow')
 clock_label.grid(row=0, column=0, sticky="nsew", pady=40)
 root.grid_columnconfigure(0, weight=1)
+
+
+world_clock = Button(root, text='World Clocks', font=('Helvetica', 24), background='black', fg='yellowgreen')
+world_clock.grid(row=1, column=0, pady=10)
+
+timer = Button(root, text='Timer', font=('Helvetica', 24), background='black', fg='yellowgreen')
+timer.grid(row=2, column=0, pady=10)
+
+alarm_clock = Button(root, text='Alarm Clock', font=('Helvetica', 24), background='black', fg='yellowgreen')
+alarm_clock.grid(row=3, column=0, pady=10)
 
 
 
