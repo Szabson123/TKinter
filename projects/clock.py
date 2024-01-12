d4f8412ea439b57ed378428fa730af6322411273
Szabson123 <szymonzaba18@gmail.com>
2024-01-12 09:08:11 +0100

    Front Clock Done

--- projects/clock.py
+++ projects/clock.py
@@ -7,14 +7,22 @@ root.geometry('600x400')
 root.config(background='black')
 
 def clock():
-  pass
+    hour = time.strftime("%H")
+    minute = time.strftime("%M")
+    second = time.strftime("%S")
+    
+    clock_label.config(text=f'{hour}:{minute}:{second}')
+    
+    clock_label.after(1000, clock)
+
+
+clock_label = Label(root, text='', font=('Helvetica', 24), background='black', fg='yellow')
+clock_label.grid(row=0, column=0, sticky="nsew", pady=40)
+root.grid_columnconfigure(0, weight=1)
 
 
 
-
-clock_label = Label(root, text='', font=('Helvetica', 24), background='grey', fg='yellow')
-clock_label.grid(row=0, column=0)
-
+clock()
 
 
 root.mainloop()
